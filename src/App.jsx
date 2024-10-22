d145558b7005d2d1a1c6743636b09a70488d169b
Thammaruthai <thammaruthai@gmail.com>
2024-10-22 14:42:39 +0700

    feat: Answer ex1 and ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import "./App.css";
+import { Button } from "./components/Button";
+import { Alert } from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
         {/* Render ตัว Button 2 แบบ */}
+        <Button type="Primary" />
+        <Button type="Secondary" />
       </div>
       <hr />
       <div className="alert-components-section">
         {/* Render ตัว Alert 4 แบบ */}
+        <Alert alertType="success" />
+        <Alert alertType="info" />
+        <Alert alertType="warning" />
+        <Alert alertType="error" />
+        <Alert alertType="asdasd" />
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,39 @@
-// Start coding here
 
-function Button() {}
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+export function Button({ type }) {
+  if (type === "Primary") {
+    return (
+      <button
+        css={css`
+          padding-right: 16px;
+          padding-left: 16px;
+          background-color: #074ee8;
+          font-size: 16px;
+          color: white;
+          width:170px;
+          heigth:50px;
+        `}
+      >
+        Button
+      </button>
+    );
+  } else {
+    return (
+      <button
+        css={css`
+          padding-right: 16px;
+          padding-left: 16px;
+          background-color: #07a4e8;
+          font-size: 16px;
+          color: white;
+          width: 170px;
+          heigth: 50px;
+        `}
+      >
+        Button
+      </button>
+    );
+  }
+}
